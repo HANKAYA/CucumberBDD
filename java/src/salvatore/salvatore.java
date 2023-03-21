db21f717381df7ea5bfd218539bffae8cda971b5
mehmetkino <mehmetkino@yahoo.com>
2023-03-21 19:23:35 -0400

    salvatore

---
+++ java/src/salvatore/salvatore.java
@@ -0,0 +1,7 @@
+package salvatore;
+
+public class salvatore {
+    public static void main(String[] args) {
+        System.out.println("Hi Eyup just checking some gitHub actions, you better accept my pull request!");
+    }
+}
